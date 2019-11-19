5222447c9d22c00224e90d81342ede174a8e312b
Giorgio Ventura <ventura.giorgio.95@gmail.com>
2019-11-19 21:14:40 +0100

    ufo

--- sketch.js
+++ sketch.js
@@ -1,11 +1,63 @@
-function preload(){
-  // put preload code here
-}
+var value = 10;
+var rotation = 1;
+var inclination = 1;
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  colorMode(HSB);
+
+  angleMode(DEGREES);
+
+  setShakeThreshold(15);
+  frameRate(12);
+
+
+
+
 }
 
 function draw() {
-  // put drawing code here
+  background(240, 78, 44);
+
+
+  rotateY(rotation);
+  rotateX(-rotation);
+
+  var dirX = (inclination / width - 0.5) * 3;
+  var dirY = (inclination / height - 0.5) * 3;
+
+
+  directionalLight(0, 0, 75, -dirX, -dirY, -0.5);
+  directionalLight(228, 91, 86, dirX, -dirY, -0.025);
+
+  ambientMaterial(255);
+
+  noStroke();
+
+  cone(value,value/2);
+
+  camera(0,0,200,0,0,0,0,1,0);
+
+
+}
+
+function deviceShaken() {
+    value = value + 1;
+    rotation = rotation + 10;
+    if (value > 50) {
+      value = 0;
+    }
+}
+
+function deviceTurned() {
+
+    inclination = inclination + 50;
+    if (value > 50) {
+      inclination = 0;
+    }
+}
+//FOR IOS: request access to motion and orientation data
+
+function touchEnded(event) {
+  DeviceOrientationEvent.requestPermission()
 }
